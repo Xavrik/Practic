ded0704794f5f8f12b8a508e23ca25f789684a42
Xavrik <anton.hohanov@gmail.com>
2025-01-13 21:43:28 +0400

    Tim Form Marketing

--- PracticOnExercism/Practic/src/main/java/org/example/exercism/TimFromMarketing/Badge.java
+++ PracticOnExercism/Practic/src/main/java/org/example/exercism/TimFromMarketing/Badge.java
@@ -1,4 +1,35 @@
 package org.example.exercism.TimFromMarketing;
 
 public class Badge {
+    public String print(Integer id, String name, String department) {
+        String badge= null;
+        if (id != null && name != null && department != null ){
+            badge = "["+ id + "] - " + name + " - " +  department.toUpperCase() ;
+        }
+        else if (id == null && name!= null && department == null){
+            badge =  name + " - OWNER";
+        }
+        else if (department == null){
+            badge =  "["+ id + "] - " +name + " - OWNER";
+        }
+        else if (id == null){
+            badge = name + " - " + department.toUpperCase();
+        }
+       return badge;
+    }
+
+    public static void main(String[] args) {
+        Badge badge = new Badge();
+//        Task1
+        System.out.println( badge.print(734, "Ernest Johnny Payne", "Strategic Communication"));
+        // Task2
+        System.out.println(badge.print(null ,"Ernest Johnny Payne", "Strategic Communication"));
+        // Task3
+        System.out.println(badge.print(null ,"Ernest Johnny Payne", null));
+        System.out.println(badge.print(59 ,"Ernest Johnny Payne", null));
+
+
+
+
+    }
 }
